5b6d35a172ff9cc8c99a1db599a00c18541a3a0e
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-12-03 11:51:20 +0100

    added parser

--- tsp.py
+++ tsp.py
@@ -1,8 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+
+def parse_file(fname):
+    """Parse input file of TSP problem to array of node coordinates
+
+    Args:
+        fname : string
+            file name of data file
+
+    Returns:
+        data : np array of lists
+            each list in the array represents a node and its coordinate
+
+    """
+    data = []
+    with open(fname) as f:
+
+        # read through file and strip first 6 header lines
+        for line in itertools.islice(f, 6, None):
+
+            # if EOF reached, stop reading input
+            if line.strip("\n") == "EOF":
+                break
+
+            # strip "\n" and split at " "
+            line = line.strip("\n").split(" ")
+
+            # remove empty strings from line list
+            line = [number for number in line if number != ""]
+
+            # convert all strings in line to float
+            data.append([int(float(i)) for i in line])
+
+    return data
 
 
 def main():
+
+    fname = "data/eil51.tsp.txt"
+    # fname = "data/a280.tsp.txt"
+    # fname = "data/pcb442.tsp.txt"
+
+    data = parse_file(fname)
+    print(data)
 
     return
 
