b29419bf42a1f28501721a46081e210d3d2e40d9
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-12-03 14:33:41 +0100

    added stripping of node number

--- parser.py
+++ parser.py
@@ -2,16 +2,18 @@ import numpy as np
 import itertools
 
 
-def parse_file(fname):
+def parse_file(fname, strip_node_num=False):
     """Parse input file of TSP problem to array of node coordinates
 
     Args:
         fname : string
             file name of data file
+        no_node_num : boolean
+            removes node number from data, returns only the coordinate per node
 
     Returns:
-        data : np array of lists
-            each list in the array represents a node and its coordinate
+        data : np array of lists ([#NODE, x, y] if strip_node_num=False, else [x, y])
+            each list in the array represents a node
 
     """
     data = []
@@ -27,8 +29,12 @@ def parse_file(fname):
             # strip "\n" and split at " "
             line = line.strip("\n").split(" ")
 
-            # remove empty strings from line list
+            # remove empty strings from line list.
             line = [number for number in line if number != ""]
+
+            # remove node number
+            if strip_node_num:
+                line = line[1:]
 
             # convert all strings in line to float
             data.append([int(float(i)) for i in line])
--- tsp.py
+++ tsp.py
@@ -6,11 +6,11 @@ import parser
 def main():
 
     fname = "data/eil51.tsp.txt"
-    # fname = "data/a280.tsp.txt"
-    # fname = "data/pcb442.tsp.txt"
+    fname = "data/a280.tsp.txt"
+    fname = "data/pcb442.tsp.txt"
 
     # parse tsp.txt file to data
-    data = parser.parse_file(fname)
+    data = parser.parse_file(fname, strip_node_num=True)
     print(data)
 
     return
