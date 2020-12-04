de346add52bd3e479b1190ce12f58499985125ed
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-12-04 14:03:40 +0100

    added distance func()

--- tsp.py
+++ tsp.py
@@ -3,15 +3,24 @@ import matplotlib.pyplot as plt
 import parser
 
 
+def distance(node):
+    if len(node) == 2:
+        return np.sqrt(node[0]**2 + node[1]**2)
+    elif len(node) == 3:
+        return np.sqrt(node[1]**2 + node[2]**2)
+
+
 def main():
 
     fname = "data/eil51.tsp.txt"
-    fname = "data/a280.tsp.txt"
-    fname = "data/pcb442.tsp.txt"
+    # fname = "data/a280.tsp.txt"
+    # fname = "data/pcb442.tsp.txt"
 
     # parse tsp.txt file to data
-    data = parser.parse_file(fname, strip_node_num=True)
-    print(data)
+    data = parser.parse_file(fname, strip_node_num=False)
+
+    print([distance(node) for node in data])
+
 
     return
 
