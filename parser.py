3d27e2756c636fc93434b58925d4c5fc4a7b7768
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-12-04 17:10:33 +0100

    implemented 2-opt and cooling schedule

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import itertools
 
 
 def parse_file(fname, strip_node_num=False):
-    """Parse input file of TSP problem to array of node coordinates
+    """Parse input file of TSP problem (tsp.txt) to array of node coordinates
 
     Args:
         fname : string
@@ -14,7 +14,6 @@ def parse_file(fname, strip_node_num=False):
     Returns:
         data : np array of lists ([#NODE, x, y] if strip_node_num=False, else [x, y])
             each list in the array represents a node
-
     """
     data = []
     with open(fname) as f:
@@ -40,3 +39,58 @@ def parse_file(fname, strip_node_num=False):
             data.append([int(float(i)) for i in line])
 
     return np.array(data)
+
+
+def parse_sol_file(fname):
+    """Parse input file of TSP solution (opt.tour.txt) to array of nodes
+
+    Args:
+        fname : string
+            file name of solution file
+
+    Returns:
+        data : np array
+            array of node numbers
+    """
+    data = []
+    with open(fname) as f:
+
+        # read through file and strip first 6 header lines
+        for line in itertools.islice(f, 5, None):
+
+            # if EOF reached, stop reading input
+            if line.strip("\n") == "EOF" or line.strip("\n") == "-1":
+                break
+
+            # strip "\n" and split at " "
+            line = line.strip("\n")
+
+            # convert all strings in line to float
+            data.append(int(line))
+
+    return np.array(data)
+
+
+def get_coords_opt_tour(fname_opt_tour, fname_tsp):
+    """Get corresponding coordinates for optimal tour file (opt.tour.txt)
+
+    Args:
+        fname_opt_tour : string
+             file name of given optimal solution (opt.tour.txt)
+        fname_tsp : string
+             file name of given problem (tsp.txt)
+
+    Returns:
+        coords_opt_tour : np array of [x, y] coordinates
+            each list in the array represents the coordinates of a node
+    """
+    nodes_tsp = parse_file(fname_tsp, strip_node_num=False)
+    nodes_opt_tour = parse_sol_file(fname_opt_tour)
+
+    coords_opt_tour = []
+    for node_num in nodes_opt_tour:
+        index = np.where(nodes_tsp[:,0] == node_num)[0][0]
+
+        coords_opt_tour.append(nodes_tsp[index][1:])
+
+    return np.array(coords_opt_tour)
