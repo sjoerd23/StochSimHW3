4c771808d1ceb55e48ba9d48bad89d3c19b67eb2
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-12-05 19:41:03 +0100

    added optimizations and significant speed-ups

--- tsp.py
+++ tsp.py
@@ -2,8 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import tqdm
+import time
+import numba
 import parser
 
+
+tot_distance_times = []
+tot_distance_sq_times = []
 
 ## TODO: implement 2-opt. Temporary swap function
 def two_opt(nodes):
@@ -64,7 +69,7 @@ def distance(node1, node2):
     return np.sqrt((node1[-2]-node2[-2])**2 + (node1[-1]-node2[-1])**2)
 
 
-def p_boltzmann(nodes, nodes_cand, t):
+def p_boltzmann(nodes_tot_distance, nodes_cand_tot_distance, t):
     """Calculate boltzmann distribution
 
     Args:
@@ -79,25 +84,47 @@ def p_boltzmann(nodes, nodes_cand, t):
         boltzmann_value : float
             value of the boltzmann distribution
     """
-    if (total_distance(nodes_cand) - total_distance(nodes)) < 0:
-        print("weird")
-    return np.exp(-(total_distance(nodes_cand) - total_distance(nodes)) / t)
+    return np.exp(-(nodes_cand_tot_distance - nodes_tot_distance) / t)
 
 
-def total_distance(nodes):
-    """Calculate total distance for the tsp in solution nodes
+@numba.njit
+def tot_distance_sq(nodes):
+    """Calculate total squared Euclidean distance for the tsp in solution nodes (for comparison of distances,
+    it is unnecessary to take square root. Inequalities will still hold without)
 
     Args:
         nodes : np array
              array of x and y coords of a solution
 
     Returns:
-        distance : float
+        total_sq : float
+            total squared distance between route in nodes
+    """
+    total_sq = 0
+    for i in range(len(nodes)-1):
+        total_sq += (nodes[i][-2]-nodes[i+1][-2])**2 + (nodes[i][-1]-nodes[i+1][-1])**2
+
+    return total_sq
+
+
+## TODO: faster method, takes up the main part of computational effort
+def tot_distance(nodes):
+    """Calculate total Euclidean distance for the tsp in solution nodes
+
+    Args:
+        nodes : np array
+             array of x and y coords of a solution
+
+    Returns:
+        total : float
             total distance between route in nodes
     """
     total = 0
+    time_start = time.time()
     for i in range(len(nodes)-1):
         total += distance(nodes[i], nodes[i+1])
+    tot_distance_times.append(time.time() - time_start)
+
     return total
 
 
@@ -134,8 +161,8 @@ def calc_dist_opt_tour(fname_opt_tour, fname_tsp):
     nodes = parser.get_coords_opt_tour(fname_opt_tour, fname_tsp)
 
     # calculate the minimal distance
-    min_dist = total_distance(nodes)
-    print("Minimum distance given solution: {}".format(min_dist))
+    min_dist = tot_distance(nodes)
+    print("Minimum distance given solution: {:.2f}".format(min_dist))
 
     return min_dist
 
@@ -144,38 +171,55 @@ def main():
 
     fname_opt_tour = "data/eil51.opt.tour.txt"
     fname_tsp = "data/eil51.tsp.txt"
+    # fname_opt_tour = "data/a280.opt.tour.txt"
+    # fname_tsp = "data/a280.tsp.txt"
+    # fname_opt_tour = "data/pcb442.opt.tour.txt"
+    # fname_tsp = "data/pcb442.tsp.txt"
 
-    # calculate distance for given best solution (opt.tour.txt files)
-    calc_dist_opt_tour(fname_opt_tour, fname_tsp)
+    # variables to time parts of the program
+    time_start = time.time()
+    tot_distance_sq_times = []
 
     # set seed for np.random module
     np.random.seed()
 
+    # calculate distance for given best solution (opt.tour.txt files)
+    calc_dist_opt_tour(fname_opt_tour, fname_tsp)
+
     # parse tsp.txt input file to node
     nodes = parser.parse_file(fname_tsp, strip_node_num=True)
+    print("Initial distance: {:.2f}".format(tot_distance(nodes)))
 
     # create random initial solution
     np.random.shuffle(nodes)
 
     # length of the markov chain
-    markov_length = len(nodes)
+    markov_length = len(nodes)    # taken as len(nodes), can be adjusted to anything else
     t_min = 1
     t0 = 10
     t = t0
     curr_iter = 0
-    print("Initial distance: {}".format(total_distance(nodes)))
 
     # outer loop, decreasing temp
     while t > t_min:
         for _ in range(markov_length):
+
             # generate candidate solution using 2-opt
             nodes_cand = two_opt(nodes.copy())
 
+            # calculate total squared distances
+            time_dist_sq_start = time.time()
+
+            nodes_tot_distance = tot_distance_sq(nodes)
+            nodes_cand_tot_distance = tot_distance_sq(nodes_cand)
+
+            tot_distance_sq_times.append(time.time() - time_dist_sq_start)
+
             # y better than x, else x better than y
-            if total_distance(nodes_cand) < total_distance(nodes):
+            if nodes_cand_tot_distance < nodes_tot_distance:
                 nodes = nodes_cand.copy()
             else:
-                p = p_boltzmann(nodes, nodes_cand, t)
+                p = p_boltzmann(nodes_tot_distance, nodes_cand_tot_distance, t)
                 r = np.random.random()
 
                 # if r < p, still take new solution even if it's worse
@@ -186,7 +230,10 @@ def main():
         t = t_schedule(curr_iter, t0)
         curr_iter += 1
 
-    print("Minimum found distance: {}".format(total_distance(nodes)))
+    print("Elapsed time due to summing squared distances: {:.2f}s".format(np.sum(tot_distance_sq_times)))
+    print("Elapsed total time: {:.2f}s".format(time.time() -time_start))
+
+    print("Minimum found distance: {:.2f}".format(tot_distance(nodes)))
 
     return
 
