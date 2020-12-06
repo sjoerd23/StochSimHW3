45fdb880d29471fa8bd4e12e6db5b20f6d594ec8
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2020-12-06 11:40:04 +0100

    performence speed-up and additional cooling schedules

--- tsp.py
+++ tsp.py
@@ -7,6 +7,7 @@ import numba
 import parser
 
 
+@numba.njit
 def two_opt(nodes):
     """2 opt
 
@@ -29,6 +30,7 @@ def two_opt(nodes):
     return nodes
 
 
+@numba.njit
 def swap(nodes):
     """swap 2 cities
 
@@ -49,6 +51,7 @@ def swap(nodes):
     return nodes
 
 
+@numba.njit
 def distance(node1, node2):
     """Calculate Euclidean distance between node1 and node2
 
@@ -65,6 +68,7 @@ def distance(node1, node2):
     return np.sqrt((node1[-2]-node2[-2])**2 + (node1[-1]-node2[-1])**2)
 
 
+@numba.njit
 def p_boltzmann(nodes_tot_distance, nodes_cand_tot_distance, t):
     """Calculate boltzmann distribution
 
@@ -103,7 +107,7 @@ def tot_distance_sq(nodes):
     return total_sq
 
 
-## TODO: faster method, takes up the main part of computational effort
+@numba.njit
 def tot_distance(nodes):
     """Calculate total Euclidean distance for the tsp in solution nodes
 
@@ -117,12 +121,13 @@ def tot_distance(nodes):
     """
     total = 0
     for i in range(len(nodes)-1):
-        total += distance(nodes[i], nodes[i+1])
+        total += np.sqrt((nodes[i][-2]-nodes[i+1][-2])**2 + (nodes[i][-1]-nodes[i+1][-1])**2)
 
     return total
 
 
-def t_schedule(curr_iter, t0):
+@numba.njit
+def t_log(curr_iter, t0):
     """Cooling schedule
 
     Args:
@@ -136,6 +141,24 @@ def t_schedule(curr_iter, t0):
             temperature
     """
     return t0 / (1 + np.log(1 + curr_iter))
+
+
+@numba.njit
+def t_over_quadr(curr_iter, t0):
+    """Cooling schedule
+
+    Args:
+        curr_iter : int
+            current iteration
+        t0 : float
+            initial temperature
+
+    Returns:
+        t : float
+            temperature
+    """
+    a=0.0000000005
+    return t0 / (1+a * curr_iter**2)
 
 
 def calc_dist_opt_tour(fname_opt_tour, fname_tsp):
@@ -156,7 +179,6 @@ def calc_dist_opt_tour(fname_opt_tour, fname_tsp):
 
     # calculate the minimal distance
     min_dist = tot_distance(nodes)
-    print("Minimum distance given solution: {:.2f}".format(min_dist))
 
     return min_dist
 
@@ -172,42 +194,44 @@ def main():
 
     # variables to time parts of the program
     time_start = time.time()
-    tot_distance_sq_times = []
 
     # set seed for np.random module
     np.random.seed()
 
     # calculate distance for given best solution (opt.tour.txt files)
-    calc_dist_opt_tour(fname_opt_tour, fname_tsp)
+    optimal_distance = calc_dist_opt_tour(fname_opt_tour, fname_tsp)
 
-    # parse tsp.txt input file to node
+    # parse tsp.txt input file to nodes
     nodes = parser.parse_file(fname_tsp, strip_node_num=True)
-    print("Initial distance: {:.2f}".format(tot_distance(nodes)))
 
     # create random initial solution
     np.random.shuffle(nodes)
+    initial_distance = tot_distance(nodes)
 
-    # length of the markov chain
-    markov_length = len(nodes)    # taken as len(nodes), can be adjusted to anything else
+    # specify parameters for SA
+    markov_length = len(nodes)*5    # taken as len(nodes), can be adjusted to anything else
     t_min = 1
-    t0 = 10
+    t0 = 11
+
     t = t0
     curr_iter = 0
+    print_counter = 0
 
-    # outer loop, decreasing temp
+    # outer loop, decreasing temperature until stopping condition is reached
     while t > t_min:
+        if print_counter % 1000 == 0:
+            print("temperature: {:.4f}".format(t))
+        print_counter += 1
+
+        # inner loop, over the Markov chain
         for _ in range(markov_length):
 
             # generate candidate solution using 2-opt
             nodes_cand = two_opt(nodes.copy())
 
-            # calculate total squared distances
-            time_dist_sq_start = time.time()
-
+            # calculate objective function
             nodes_tot_distance = tot_distance_sq(nodes)
             nodes_cand_tot_distance = tot_distance_sq(nodes_cand)
-
-            tot_distance_sq_times.append(time.time() - time_dist_sq_start)
 
             # y better than x, else x better than y
             if nodes_cand_tot_distance < nodes_tot_distance:
@@ -220,13 +244,14 @@ def main():
                 if r < p:
                     nodes = nodes_cand.copy()
 
-        # calculate new t
-        t = t_schedule(curr_iter, t0)
+        # calculate new temperature
+        t = t_log(curr_iter, t0)
         curr_iter += 1
 
-    print("Elapsed time due to summing squared distances: {:.2f}s".format(np.sum(tot_distance_sq_times)))
-    print("Elapsed total time: {:.2f}s".format(time.time() -time_start))
+    print("\nElapsed time: {:.2f}s".format(time.time() -time_start))
 
+    print("Minimum distance given solution: {:.2f}".format(optimal_distance))
+    print("Initial distance: {:.2f}".format(initial_distance))
     print("Minimum found distance: {:.2f}".format(tot_distance(nodes)))
 
     return
